d5f1dc13890a93a964df2b99481215d66410f1ac
seglass5 <s.glass.cls@gmail.com>
2020-10-12 13:30:43 +0100

    Update dot.py
    Added sam functionality to variables

--- torchviz/dot.py
+++ torchviz/dot.py
@@ -41,16 +41,16 @@ def make_dot(var, params=None):
             if torch.is_tensor(var):
                 # note: this used to show .saved_tensors in pytorch0.2, but stopped
                 # working as it was moved to ATen and Variable-Tensor merged
-                dot.node(str(id(var)), size_to_str(var.size()), fillcolor='orange')
+                dot.node(str(id(var)), var.sam, size_to_str(var.size()), fillcolor='orange')
             elif hasattr(var, 'variable'):
                 u = var.variable
                 name = param_map[id(u)] if params is not None else ''
                 node_name = '%s\n %s' % (name, size_to_str(u.size()))
-                dot.node(str(id(var)), node_name, fillcolor='lightblue')
+                dot.node(str(id(var)), var.sam, node_name, fillcolor='lightblue')
             elif var in output_nodes:
-                dot.node(str(id(var)), str(type(var).__name__), fillcolor='darkolivegreen1')
+                dot.node(str(id(var)), var.sam, str(type(var).__name__), fillcolor='darkolivegreen1')
             else:
-                dot.node(str(id(var)), str(type(var).__name__))
+                dot.node(str(id(var)), var. sam, str(type(var).__name__))
             seen.add(var)
             if hasattr(var, 'next_functions'):
                 for u in var.next_functions:
